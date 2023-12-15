820bcd2eea8e2bd159360953885e1453c4031b25
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-12-15 20:56:22 +0900

    [Silver III] Title: N과 M (1), Time: 292 ms, Memory: 22436 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,32 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-//bufferedreader+stringbuffer 사용
+
 
 public class Main {
 	
 	private static StringBuffer sb=new StringBuffer(); 
 	private static int[] arr;
 	private static boolean[] visited;
+	private static int N,M;
 
-	public static void DFS(int n, int m, int depth) throws IOException {
-		if(depth==m+1) {
-			for(int i=1;i<m+1;i++)
+	public static void DFS(int depth) throws IOException {
+		if(depth==M+1) {
+			for(int i=1;i<M+1;i++)
 				sb.append(arr[i]).append(" ");
 			sb.append("\n");
 			
 			return;
 		}
 		else {
-			for(int i=1;i<n+1;i++) {
+			for(int i=1;i<N+1;i++) {
 				if(!visited[i]) {
 					visited[i]=true;
 					arr[depth]=i;
-					DFS(n,m,depth+1);
+					DFS(depth+1);
 					visited[i]=false;
 				}
 			}
@@ -40,16 +39,15 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		br.close();
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
 		visited= new boolean[N+1];
 		Arrays.fill(visited,false);
 		arr= new int[M+1];
 		
-		DFS(N,M,1);
+		DFS(1);
 		System.out.println(sb);
-		 
 		
 	}
 }
