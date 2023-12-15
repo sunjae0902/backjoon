ed1d16f25e7024a9c6af49c2700faca7fb6bd13d
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-12-15 20:44:26 +0900

    [Silver III] Title: N과 M (1), Time: 340 ms, Memory: 31180 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+	private static int[] arr;
+	private static boolean[] visited;
+
+	public static void DFS(int n, int m, int depth) throws IOException {
+		if(depth==m+1) {
+			for(int i=1;i<m+1;i++)
+				bw.write(arr[i]+" ");
+			bw.write("\n");
+			return;
+		}
+		else {
+			for(int i=1;i<n+1;i++) {
+				if(!visited[i]) {
+					visited[i]=true;
+					arr[depth]=i;
+					DFS(n,m,depth+1);
+					visited[i]=false;
+				}
+			}
+		}
+	
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		br.close();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		visited= new boolean[N+1];
+		Arrays.fill(visited,false);
+		arr= new int[M+1];
+		
+		DFS(N,M,1);
+		bw.close();  
+		
+	}
+}
