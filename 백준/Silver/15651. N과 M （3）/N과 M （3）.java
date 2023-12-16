9545d9c7dcde9dbbf29eef70346f38303515f8cb
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-12-16 15:11:49 +0900

    [Silver III] Title: N과 M (3), Time: 700 ms, Memory: 71624 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	private static StringBuffer sb=new StringBuffer(); 
+	private static int[] arr;
+	private static int N,M;
+
+	public static void DFS(int depth) throws IOException {
+		if(depth==M+1) {
+			for(int i=1;i<M+1;i++)
+				sb.append(arr[i]).append(" ");
+			sb.append("\n");
+			return;
+		}
+		else {
+			for(int i=1;i<N+1;i++) {
+                arr[depth]=i;
+				DFS(depth+1);
+				
+			}
+		}
+	
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+	
+		br.close();
+		Scanner sc=new Scanner(System.in);
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr= new int[M+1];
+		
+		DFS(1);
+		System.out.println(sb);
+		
+	}
+}
