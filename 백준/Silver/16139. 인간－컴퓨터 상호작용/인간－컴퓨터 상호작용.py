49a6b644381dd719b3db804fd29be38219f2b138
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2024-09-23 20:25:32 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 48 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+s = input().strip()  # 문자열 끝 공백 제거
+q = int(input())     # 쿼리 수 입력
+
+cnt = [[0] * 26 for _ in range(len(s) + 1)]
+
+for i in range(len(s)):
+    c = ord(s[i]) - 97  # 현재 문자의 알파벳 인덱스 (0 ~ 25)
+    for j in range(26):
+        cnt[i+1][j] = cnt[i][j] # 다음 행 리스트에 이전 행 리스트 복사
+    cnt[i+1][c] += 1 # 갱신
+
+for _ in range(q):
+    a, l, r = input().split()
+    a = ord(a) - 97  # 알파벳을 숫자로 변환
+    l = int(l)
+    r = int(r)
+
+    # r까지의 누적합에서 l-1까지의 누적합을 빼기
+    print(cnt[r+1][a] - cnt[l][a])
