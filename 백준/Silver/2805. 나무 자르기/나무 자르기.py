125999e0321fc4feb6d709d99bea104c98b85c51
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2024-11-23 19:58:46 +0900

    [Silver II] Title: 나무 자르기, Time: 440 ms, Memory: 258048 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+tree = sorted(list(map(int, input().split())))
+
+def binarySearch(start, end):
+    while(start <= end):
+        mid = (start + end) // 2
+        tmp = 0
+        for i in range(N):
+            if tree[i] > mid:
+                tmp += tree[i] - mid
+        if tmp >= M:
+            start = mid + 1
+        else:
+            end = mid -1
+    return end
+            
+max = binarySearch(0, tree[N-1])
+print(max)  
