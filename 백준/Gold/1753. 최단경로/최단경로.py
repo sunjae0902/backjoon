0abe45ce246ffc05489b18bdd6eabbb01c9a0eb3
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2024-12-01 02:27:05 +0900

    [Gold IV] Title: 최단경로, Time: 684 ms, Memory: 68220 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,33 @@
+import sys, heapq
+input = sys.stdin.readline
+INF = int(1e9)
+
+V, E = map(int, input().split())
+S = int(input())
+arr = [[] for _ in range(V + 1)]
+distances = [INF] * (V+1)
+
+for i in range(E):
+    a, b, c = map(int, input().split())
+    if len(arr[a]) > 0 and arr[a][0][0] == b and arr[a][0][1] > c:
+        arr[a][0] = (b,c)
+    else:
+        arr[a].append((b,c))
+    
+
+def dijkstra(s):
+    q = []
+    heapq.heappush(q, (0, s))
+    distances[s] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distances[now] < dist:
+            continue
+        for i, c in arr[now]:
+            cost = distances[now] + c
+            if cost < distances[i]:
+                distances[i] = cost
+                heapq.heappush(q, (cost, i))
+dijkstra(S)
+for i in range(1, V+1):
+    print(distances[i] if distances[i] != INF else "INF")
