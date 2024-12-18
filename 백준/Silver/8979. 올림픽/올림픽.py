546ebed8fc20491dae5acf384c364709b15167ed
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2024-12-18 18:34:42 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 32412 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+arr = []
+rank = [0 for _ in range(N+1)] # i번 국가의 순서
+for i in range(N):
+    arr.append(list(map(int, input().split())))
+arr.sort(reverse=True, key = lambda x: (x[1], x[2], x[3]))
+
+rank[arr[0][0]] = 1
+for i in range(1, len(arr)):
+    if arr[i][1:] == arr[i-1][1:]:
+        rank[arr[i][0]] = rank[arr[i-1][0]]
+    else:
+        rank[arr[i][0]] = rank[arr[i-1][0]] + 1
+print(rank[K])
+    
+    
