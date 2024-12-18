31f3b992dfa93280587d51df964f71a6e1bb95f4
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2024-12-18 19:01:57 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -9,11 +9,12 @@ for i in range(N):
 arr.sort(reverse=True, key = lambda x: (x[1], x[2], x[3]))
 
 rank[arr[0][0]] = 1
+cnt = 1
 for i in range(1, len(arr)):
     if arr[i][1:] == arr[i-1][1:]:
         rank[arr[i][0]] = rank[arr[i-1][0]]
     else:
-        rank[arr[i][0]] = rank[arr[i-1][0]] + 1
+        rank[arr[i][0]] = cnt + 1     
+    cnt += 1
 print(rank[K])
-    
     
