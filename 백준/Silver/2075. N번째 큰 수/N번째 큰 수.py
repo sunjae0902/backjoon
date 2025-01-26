21472be18cae43e6838e50b71970d78813afd67d
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-01-26 13:51:13 +0900

    [Silver III] Title: N번째 큰 수, Time: 648 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 N = int(input())
 hq = []
 # 우선순위 큐, (각 열이 정렬됨, 큐 사이즈 유지하며 비교하기)
-
+# 메모리 초과 주의
 for i in range(N):
     arr = list(map(int, input().split())) 
     if i == 0:
@@ -11,10 +11,7 @@ for i in range(N):
             heapq.heappush(hq, j)
         continue
     for j in arr:
-        if len(hq) < N:
-            heapq.heappush(hq, j)
-        # hq[0]: 루트 노드 확인하기
-        elif hq[0] < j: # 큐의 최솟값보다 크면 푸시.
+       if hq[0] < j: # 큐의 최솟값보다 크면 푸시.
             heapq.heappush(hq, j) # 정렬 유지
             heapq.heappop(hq)
 print(heapq.heappop(hq))
