1036e02e94edcb70b0e5300fe0b3528bfd3e49af
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-03-13 20:22:30 +0900

    [level 2] Title: 소수 찾기, Time: 11.56 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -4,8 +4,6 @@ import math
 def is_prime(num):
     if num <= 1:
         return False
-    elif num == 2:
-        return True
     for i in range(2, int(math.sqrt(num))+1):
         if num % i == 0:
             return False
