04a4efaa404df247d2af9a7aef5c13bc35de2ccb
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-03-18 23:19:10 +0900

    [level 3] Title: 이중우선순위큐, Time: 503.60 ms, Memory: 15.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -8,22 +8,17 @@ def solution(operations):
         if oper[0] == 'I':
             heapq.heappush(max_hq, (-1) * int(oper[1]))
             heapq.heappush(min_hq, int(oper[1]))
-            
         elif oper[1] == '-1':
             if not min_hq:
                 continue
             minimum = heapq.heappop(min_hq)
             if -minimum in max_hq:
                 max_hq.remove(-minimum)
-                heapq.heapify(max_hq)
-    
         else:
             if not max_hq:
                 continue
             maximum = -heapq.heappop(max_hq)
             if maximum in min_hq:
                 min_hq.remove(maximum)
-                heapq.heapify(min_hq)
-                  
-    
+                
     return [-max_hq[0], min_hq[0]] if max_hq and min_hq else [0,0]
