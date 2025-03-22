3ee148ae3670f0209b853a977924250dd955ef35
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-03-22 23:08:07 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,29 +1,26 @@
 import heapq
 
 def solution(n, costs):
+    answer = 0
     arr = [[] for _ in range(n)]
     for s, e, w in costs:
-        arr[s].append((w, e))
-        arr[e].append((w, s))
-
-    def prim():
-        hq = [(0, 0)]  # (가중치, 시작 노드)
-        visited = [0] * n
-        total_weight = 0
-        count = 0  # 간선 개수
-        
-        while hq:
-            w, node = heapq.heappop(hq)
-            if visited[node]:
-                continue
-            visited[node] = True
-            total_weight += w
-            count += 1
-            if count == n:  # 모든 노드가 연결되면 종료
-                break
-            for nw, next_node in arr[node]:
-                if not visited[next_node]:
-                    heapq.heappush(hq, (nw, next_node))     
-        return total_weight
-
-    return prim()
+        arr[s].append([e,w])
+        arr[e].append([s,w])
+    
+    visited = set()
+    q = [[0, 0]] # 가중치, 시작 노드
+    count = 0
+    while q:
+        weight, now = heapq.heappop(q)
+        if now in visited:
+            continue
+        visited.add(now) # 가장 최소인 노드 방문 표시.
+        answer += weight
+        count += 1
+        if count == n:
+            break
+        for v, w in arr[now]:
+            if v not in visited:
+                heapq.heappush(q, [w, v])
+    
+    return answer
