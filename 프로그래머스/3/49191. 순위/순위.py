285f93411d6ce868ee608fdd2f0bf9c3f329871c
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-03-29 02:18:41 +0900

    [level 3] Title: 순위, Time: 86.69 ms, Memory: 9.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,20 +1,18 @@
 def solution(n, results):
-    graph = [[0] * (n + 1) for _ in range(n + 1)]
-
-    for win, lose in results:
-        graph[win][lose] = 1
-        graph[lose][win] = -1
-        
-    # 플로이드-워셜 
-    for k in range(1, n + 1):
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                if graph[i][k] == 1 and graph[k][j] == 1:
-                    graph[i][j] = 1  # i가 j를 이김
-                    graph[j][i] = -1 # j가 i에게 짐
     answer = 0
-    for i in range(1, n + 1):
-        count = sum(1 for j in range(1, n + 1) if graph[i][j] != 0)
-        if count == n - 1: # 이긴 + 진 수가 n-1이면 확정 가능
+    arr = [[0 for _ in range(n+1)] for _ in range(n+1)]
+    for a, b in results:
+        arr[a][b] = 1
+        arr[b][a] = -1 
+        
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            for k in range(1, n+1):
+                if arr[j][i] == 1 and arr[i][k] == 1:
+                    arr[j][k] = 1
+                    arr[k][j] = -1
+    for i in range(1, n+1):
+        w, l = arr[i].count(1), arr[i].count(-1)
+        if w + l == n-1:
             answer += 1
     return answer
