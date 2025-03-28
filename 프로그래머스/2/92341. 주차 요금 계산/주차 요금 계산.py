7642092ef878619eda04377b9839b9a91f9ee48f
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-03-28 21:13:39 +0900

    [level 2] Title: 주차 요금 계산, Time: 2.26 ms, Memory: 9.79 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+def get_duration(start, end):
+    start_h, start_m = int(start[:2]) * 60, int(start[3:])
+    end_h, end_m = int(end[:2]) * 60, int(end[3:])
+    return end_h + end_m - start_h - start_m
+    
+def solution(fees, records):
+    answer = []
+    times = defaultdict(int)
+    cars = defaultdict(str)
+    
+    for record in records:
+        t, n, flag = list(record.split())
+        if flag == 'IN':
+            cars[n] = t
+        else:
+            times[n] += get_duration(cars[n], t)
+            cars[n] = ''
+            
+    # 남은 시간 처리, 23:59 출차
+    for n, t in cars.items():
+        if t != '':
+            times[n] += get_duration(t, "23:59")
+            
+    answer = sorted([[n, t] for n, t in times.items()], key = lambda x: x[0])
+    for i in range(len(answer)):
+        fee = fees[1]
+        if answer[i][1] > fees[0]:
+            unit_time_fee = (answer[i][1] - fees[0]) // fees[2] if (answer[i][1] - fees[0]) % fees[2] == 0 else (answer[i][1] - fees[0]) // fees[2] + 1
+            fee += unit_time_fee * fees[3]
+        answer[i] = fee
+    return answer
