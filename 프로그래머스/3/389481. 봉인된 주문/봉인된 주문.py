0e4757048228c9668721ab12142ff2f1431afe11
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-03-29 20:48:43 +0900

    [level 3] Title: 봉인된 주문, Time: 955.25 ms, Memory: 50.4 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.py
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.py
@@ -1,25 +1,23 @@
-def get_10_from_26(s):
-    n = 0
-    for char in s:
-        n = n * 26 + (ord(char) - ord('a')) + 1
-    return n
+def to_10(s):
+    res = 0
+    for i in s:
+        res = res * 26 + ord(i) - ord('a') + 1
+    return res
 
-def get_26_from_10(n):
-    result = []
+def to_26(n):
+    res = ''
     while n > 0:
         n -= 1
-        result.append(chr(ord('a') + (n % 26)))
+        res += chr(97 + n % 26)
         n //= 26
-    return ''.join(result[::-1])
+    return res[::-1]
 
 def solution(n, bans):
-    bans_to_ten = []
-    for b in bans:
-        bans_to_ten.append(get_10_from_26(b))
-    bans_to_ten.sort()
-    for b in bans_to_ten:
+    remove = sorted([to_10(b) for b in bans])
+    cnt = 0
+    for b in remove:
         if b <= n:
-            n += 1 # 갱신
+            n += 1
         else:
-            break 
-    return get_26_from_10(n)
+            break
+    return to_26(n)
