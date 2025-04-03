5c28019bbb6763ac6b8d65f778f6339928e9376d
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-04-03 17:32:50 +0900

    [level 4] Title: [3차] 자동완성, Time: 261.21 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.py
+++ 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.py
@@ -1,23 +1,15 @@
 def solution(words):
-    trie = {}
-    answer = 0
-
-    for word in words:
-        node = trie
-        for char in word:
-            if char not in node:
-                node[char] = {"#": 0} 
-            node = node[char]
-            node["#"] += 1
+    answer = [0] * len(words)
+    words.sort()
     
-    for word in words:
-        node = trie
-        for i, char in enumerate(word):
-            node = node[char]
-            if node["#"] == 1:  
-                answer += i + 1
+    for i in range(len(words)-1):
+        a, b = len(words[i]), len(words[i+1])
+        for j in range(min(a, b)):
+            if words[i][j] != words[i+1][j]: # 불일치
+                j -= 1 # 마지막으로 일치하는 자리
                 break
-        else:
-            answer += len(word)
 
-    return answer
+        answer[i] = max(answer[i], min(j+2, a))
+        answer[i+1] = max(answer[i+1], min(j+2, b))
+        
+    return sum(answer)
