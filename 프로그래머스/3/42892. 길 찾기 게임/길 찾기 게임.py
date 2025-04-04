8de50c2926c1418eaa071d9f10bb71492f214a4a
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-04-05 04:15:53 +0900

    [level 3] Title: 길 찾기 게임, Time: 559.65 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,26 +1,28 @@
 import sys
 sys.setrecursionlimit(10**6)
 
+def find_subtree(tree, node):
+    left = [n for n in tree if n[1] < node[1] and n[2] < node[2]]
+    right = [n for n in tree if n[1] > node[1] and n[2] < node[2]]
+    return left, right
+
 def preorder(tree, node): # 루트 - 왼 - 오
     result = [node[0]]
-    l_subtree=[n for n in tree if n[1] < node[1] and n[2] < node[2]]
-    r_subtree=[n for n in tree if n[1] > node[1] and n[2] < node[2]]
-    if l_subtree:
-        result += preorder(l_subtree, l_subtree[0])
-    if r_subtree:
-        result += preorder(r_subtree, r_subtree[0])
+    left, right = find_subtree(tree, node)
+    if left:
+        result += preorder(left, left[0])
+    if right:
+        result += preorder(right, right[0])
     return result
     
 def postorder(tree, node): # 왼 - 오 - 루트
     result = []
-    l_subtree=[n for n in tree if n[1] < node[1] and n[2] < node[2]]
-    r_subtree=[n for n in tree if n[1] > node[1] and n[2] < node[2]]
-    if l_subtree:
-        result += postorder(l_subtree, l_subtree[0])
-    if r_subtree:
-        result += postorder(r_subtree, r_subtree[0])
-        
-    result.append(node[0])
+    left, right = find_subtree(tree, node)
+    if left:
+        result += postorder(left, left[0])
+    if right:
+        result += postorder(right, right[0])
+    result += [node[0]]
     return result 
 
 def solution(nodeinfo):
