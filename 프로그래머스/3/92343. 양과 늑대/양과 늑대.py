d9199d2150916132f258ea28de650afa2524118c
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-04-05 02:28:10 +0900

    [level 3] Title: 양과 늑대, Time: 906.86 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,43 +1,33 @@
 from collections import deque
 
-def bfs(info, graph):
-    max_sheep = 0
-    queue = deque([(0, 1, 0, [0])])  # (현재 노드, 양 수, 늑대 수, 방문 경로)
-
-    while queue:
-        node, sheep, wolf, visited = queue.popleft()
-        max_sheep = max(max_sheep, sheep)
+def bfs(info, tree):
+    max_sheep = 1
+    q = deque([(0, 1, 0, [0])]) # 현재노드, 양의 수, 늑대수, 현재 경로
+    
+    while q:
+        now, s, w, path = q.popleft()
+        max_sheep = max(max_sheep, s)
         
-        # 현재 노드에서 갈 수 있는 경로 탐색
-        for next_node in graph[node]:
-            if next_node not in visited:
-                new_visited = visited + [next_node]
-                next_sheep = sheep + (1 if info[next_node] == 0 else 0)
-                next_wolf = wolf + (1 if info[next_node] == 1 else 0)
-
-                if next_sheep > next_wolf:
-                    queue.append((next_node, next_sheep, next_wolf, new_visited))
-
-        # 이전 경로로 다시 돌아가서 또 다른 노드 탐색
-        for v in visited:
-            for next_node in graph[v]:
-                if next_node not in visited:
-                    new_visited = visited + [next_node]
-                    next_sheep = sheep + (1 if info[next_node] == 0 else 0)
-                    next_wolf = wolf + (1 if info[next_node] == 1 else 0)
-
-                    if next_sheep > next_wolf:
-                        queue.append((next_node, next_sheep, next_wolf, new_visited))
-
+        for v in tree[now]:
+            if v not in path:
+                ns, nw = s + (1 if info[v] == 0 else 0), w + (1 if info[v] == 1 else 0)
+                if ns > nw:
+                    q.append((v, ns, nw, path + [v]))
+        
+        for pv in path:
+            for v in tree[pv]:
+                if v not in path:
+                    ns, nw = s + (1 if info[v] == 0 else 0), w + (1 if info[v] == 1 else 0)
+                    if ns > nw:
+                        q.append((v, ns, nw, path + [v]))
+        
     return max_sheep
+        
 
 def solution(info, edges):
-    n = len(info)
-    graph = [[] for _ in range(n)]
-
-    for parent, child in edges:
-        graph[parent].append(child)
-        graph[child].append(parent)
-
-    answer = bfs(info, graph)
-    return answer
+    tree = [[] for _ in range(len(info))]
+    for p, c in edges:
+        tree[p].append(c)
+        tree[c].append(p)
+    
+    return bfs(info, tree)
