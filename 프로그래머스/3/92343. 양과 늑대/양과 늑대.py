5e1077400146269e3c7f2c19a6b09c635d032993
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-04-05 02:44:52 +0900

    [level 3] Title: 양과 늑대, Time: 27.64 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,33 +1,21 @@
-from collections import deque
-
-def bfs(info, tree):
-    max_sheep = 1
-    q = deque([(0, 1, 0, [0])]) # 현재노드, 양의 수, 늑대수, 현재 경로
+def solution(info, edges):
+    answer = 0
+    tree = [[] for _ in range(len(info))]
     
-    while q:
-        now, s, w, path = q.popleft()
-        max_sheep = max(max_sheep, s)
-        
-        for v in tree[now]:
-            if v not in path:
-                ns, nw = s + (1 if info[v] == 0 else 0), w + (1 if info[v] == 1 else 0)
-                if ns > nw:
-                    q.append((v, ns, nw, path + [v]))
-        
+    for p, c in edges:
+        tree[p].append(c)
+        tree[c].append(p)
+    
+    def dfs(node, s, w, path):
+        nonlocal answer
+        answer = max(answer, s)
         for pv in path:
             for v in tree[pv]:
                 if v not in path:
                     ns, nw = s + (1 if info[v] == 0 else 0), w + (1 if info[v] == 1 else 0)
                     if ns > nw:
-                        q.append((v, ns, nw, path + [v]))
-        
-    return max_sheep
-        
+                        dfs(v, ns, nw, path + [v])
+       
 
-def solution(info, edges):
-    tree = [[] for _ in range(len(info))]
-    for p, c in edges:
-        tree[p].append(c)
-        tree[c].append(p)
-    
-    return bfs(info, tree)
+    dfs(0, 1, 0, [0])
+    return answer
