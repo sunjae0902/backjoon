668c6e838424f099c33c73af0d6e519dbd7e74c1
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-04-05 18:48:46 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.60 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -1,33 +1,25 @@
-def to_string(m):
-    return f'{m // 60:02d}:{m % 60:02d}' # 형식 지정 문자열
+def tomin(s):
+    return 60 * int(s[0:2]) + int(s[3:])
 
-def to_min(s):
-    h, m = map(int, s.split(':'))
-    return h * 60 + m
+def tostr(m):
+    return f'{m//60:02d}:{m%60:02d}'
 
 def solution(n, t, m, timetable):
-    timetable = sorted(to_min(time) for time in timetable)
+    answer = ''
+    timetable.sort(key = lambda x: tomin(x))
+    arrival = 60 * 9
+    time, idx = 0, 0
     
-    bus_time = 60 * 9 # 시작
-    last_arrival = 0
-    idx = 0
-    
-    for i in range(n): # n번 반복 
-        # 현재 버스에 탈 수 있는 최대 인원 계산
-        count = 0
-        while idx < len(timetable) and timetable[idx] <= bus_time and count < m:
-            last_arrival = timetable[idx]
-            count += 1
+    for i in range(n): #n번 반복 
+        people = 0
+        while people < m and idx < len(timetable) and arrival >= tomin(timetable[idx]):
+            time = tomin(timetable[idx])
             idx += 1
-        
-        # 마지막 버스
-        if i == n - 1:
-            if count < m:
-                return to_string(bus_time)
+            people += 1
+        if i == n-1: # 마지막 버스
+            if people < m:
+                answer = tostr(arrival)
             else:
-                # 자리가 없다면 마지막으로 탄 사람보다 1분 먼저 와야 함
-                return to_string(last_arrival - 1)
-
-        bus_time += t
-
-    return to_string(bus_time)
+                answer = tostr(time-1)
+        arrival += t
+    return answer
