eb6a1b53c14dd2a0d70209004ecbd63d67cd3cc2
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-04-12 01:38:44 +0900

    [level 3] Title: 야근 지수, Time: 924.80 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,14 +1,12 @@
 import heapq
-
 def solution(n, works):
-    if sum(works) <= n:
-        return 0
     works = [-w for w in works]
     heapq.heapify(works)
-    # 작업량이 많은 것부터 처리
-
-    for _ in range(n): # 총 n시간 선택
-        max_w = heapq.heappop(works)
-        heapq.heappush(works, max_w+1) # 1시간씩 줄임
-   
-    return sum(w**2 for w in works)
+    for i in range(n):
+        if not works:
+            break
+        now = heapq.heappop(works)
+        if -now > 1:
+            heapq.heappush(works, now+1)
+        
+    return sum([(-i)**2 for i in works])
