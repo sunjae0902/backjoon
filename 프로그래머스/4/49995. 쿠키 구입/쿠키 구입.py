785871ab4181f7bfaf1bb337eb8b9f37d0566d70
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-04-12 22:13:06 +0900

    [level 4] Title: 쿠키 구입, Time: 1216.47 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/4/49995. 쿠키 구입/쿠키 구입.py
+++ 프로그래머스/4/49995. 쿠키 구입/쿠키 구입.py
@@ -1,19 +1,19 @@
 def solution(cookie):
-    cookies = [0,0]
+    left, right = 0, 0
     n = len(cookie)
     for m in range(n-1):
-        left, right = m, m+1
-        left_sum, right_sum = cookie[m], cookie[m+1]
-        while left >= 0 and right < n:
-            if left_sum == right_sum and (left_sum > cookies[0] or right_sum > cookies[1]):
-                cookies = [left_sum, right_sum]
+        l, r = m, m+1 # 반대 방향으로
+        left_sum, right_sum = cookie[l], cookie[r]
+        while l >= 0 and r < n:
+            if left_sum == right_sum and (left < left_sum or right < right_sum):
+                left, right = left_sum, right_sum
                 continue
-            if left_sum <= right_sum and left > 0:
-                left -= 1
-                left_sum += cookie[left]
-            elif right < n-1:
-                right += 1
-                right_sum += cookie[right]
+            if l > 0 and left_sum <= right_sum:
+                l -= 1
+                left_sum += cookie[l]
+            elif r < n-1:
+                r += 1
+                right_sum += cookie[r]
             else:
-                break
-    return max(cookies) if all(c for c in cookies) else 0
+                break    
+    return max(left, right) if left != 0 and right != 0 else 0
