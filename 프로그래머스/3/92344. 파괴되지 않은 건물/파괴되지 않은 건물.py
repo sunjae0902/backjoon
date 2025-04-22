3f694fdd7d5b50edef6c68c50c15a2269124c504
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-04-22 22:40:36 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 856.13 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,26 +1,24 @@
 def solution(board, skill):
     answer = 0
-    N, M = len(board), len(board[0])
-    prefix_sum = [[0 for _ in range(M+1)] for _ in range(N+1)]
+    n, m = len(board), len(board[0])
+    prefix = [[0 for _ in range(m+1)] for _ in range(n+1)]
     
     for t, r1, c1, r2, c2, d in skill:
-        prefix_sum[r1][c1] += -d if t == 1 else d
-        prefix_sum[r1][c2+1] += d if t == 1 else -d
-        prefix_sum[r2+1][c1] += d if t == 1 else -d
-        prefix_sum[r2+1][c2+1] += -d if t == 1 else d
+        prefix[r1][c1] += -d if t == 1 else d
+        prefix[r2+1][c2+1] += -d if t == 1 else d
+        prefix[r1][c2+1] += d if t == 1 else -d
+        prefix[r2+1][c1] += d if t == 1 else -d
         
-    # 행 누적합
-    for i in range(N):
-        for j in range(M):
-            prefix_sum[i][j+1] += prefix_sum[i][j]
-    # 열 누적합
-    for j in range(M):
-        for i in range(N):
-            prefix_sum[i+1][j] += prefix_sum[i][j]
+    for i in range(n): #행
+        for j in range(m):
+            prefix[i+1][j] += prefix[i][j]
             
-    for i in range(N):
-        for j in range(M):
-            if board[i][j] + prefix_sum[i][j] > 0:
+    for i in range(m): #열
+        for j in range(n):
+            prefix[j][i+1] += prefix[j][i]
+    
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] + prefix[i][j] > 0:
                 answer += 1
-              
     return answer
