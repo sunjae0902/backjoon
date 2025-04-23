2c8b07d32e4dbedf103d9bd616855897752566be
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-04-23 17:57:26 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 942.49 ms, Memory: 59.4 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,10 +1,19 @@
-
 def solution(s):
-    answer = 0
-    
-    for i in range(len(s), 0, -1):
+    answer = 1
+    dp = [[0 for _ in range(len(s))] for _ in range(len(s))] # 부분 문자열 dp[i..j] 에서 가장 긴 펠린드롬 길이
+          
+    for i in range(len(s)):
+        dp[i][i] = 1
+        
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            dp[i][i+1] = 1
+            answer = 2
+  
+    for i in range(3, len(s)+1):
         for j in range(len(s)-i+1):
-            sub = s[j:j+i]
-            if sub == sub[::-1]:
-                return len(sub)
+            e = j+i-1
+            if dp[j+1][e-1] and s[j] == s[e]:
+                dp[j][e] = 1
+                answer = max(answer, i)
     return answer
