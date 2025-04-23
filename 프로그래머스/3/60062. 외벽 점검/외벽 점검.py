c32c5687ea09a645923a91b6c0c6e1533ca2a22a
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-04-23 16:07:39 +0900

    [level 3] Title: 외벽 점검, Time: 2812.64 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.py
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.py
@@ -1,23 +1,22 @@
 from itertools import permutations
-
+        
 def solution(n, weak, dist):
-    weak_len = len(weak)
-    weak_extended = weak + [w + n for w in weak]  # 선형화
-    answer = len(dist) + 1  
-    
-    for i in range(1, len(dist) + 1): 
-        for friends in permutations(dist, i):
-            for start in range(weak_len):  # 시작점
+    answer = len(dist)+1
+    weak += [w + n for w in weak]
+
+    for i in range(1, len(dist)+1):
+        for perm in permutations(dist, i):
+            for start in range(len(weak)//2): # 취약 지점에서 출발
                 count = 0
-                position = weak_extended[start] + friends[count]  # 처음에 커버 가능한 최대 위치
+                pos = weak[start] + perm[count]
                 
-                for idx in range(start, start + weak_len):
-                    if weak_extended[idx] > position: # 커버 못 함
+                for idx in range(start, start + len(weak) // 2):
+                    if weak[idx] > pos:
                         count += 1
                         if count >= i:
                             break
-                        position = weak_extended[idx] + friends[count]
-                else: # for문이 끝까지 돌았을 경우(모든 지점 탐지) 실행
-                    answer = min(answer, i)
-
+                        pos = weak[idx] + perm[count]
+                else:
+                    answer = min(answer, i)  
+                               
     return answer if answer <= len(dist) else -1
