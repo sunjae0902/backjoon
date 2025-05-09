2879c9cace25dc726a531119be30bb3674bdb8e7
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-05-09 20:18:26 +0900

    [level 2] Title: 호텔 대실, Time: 3.54 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,33 +1,17 @@
 import heapq
 
 def to_min(s):
-    return 60 * int(s[:2]) + int(s[3:])
+    return int(s[:2]) * 60 + int(s[3:])
 
 def solution(book_time):
-    answer = 0
-    book_time.sort(key=lambda x: to_min(x[0]))  # 시작 시간 기준으로 정렬
+    answer = 1
+    book_time.sort(key = lambda x: to_min(x[0]))
+    
     hq = []
-
     for s, e in book_time:
-        s_min = to_min(s)
-        e_min = to_min(e)
-        temp = []
-        assigned = False
-        
-        while hq:
-            now = heapq.heappop(hq)
-            if now + 10 <= s_min:
-                heapq.heappush(hq, e_min)
-                assigned = True
-                break
-            else:
-                temp.append(now)
-        
-        for t in temp: # 복구
-            heapq.heappush(hq, t)
-        
-        if not assigned: # 체크
-            heapq.heappush(hq, e_min)
-            answer += 1
-
+        s_min, e_min = to_min(s), to_min(e)
+        while hq and hq[0] + 10 <= s_min:
+            heapq.heappop(hq)
+        heapq.heappush(hq, e_min)
+        answer = max(answer, len(hq))
     return answer
