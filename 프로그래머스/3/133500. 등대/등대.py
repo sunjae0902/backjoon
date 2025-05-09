a80f0f2538db8cf20a0fdd9d51a10b9ddd690c58
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-05-09 19:39:30 +0900

    [level 3] Title: 등대, Time: 242.23 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.py
+++ 프로그래머스/3/133500. 등대/등대.py
@@ -1,26 +1,22 @@
 import sys
-from collections import defaultdict
 sys.setrecursionlimit(100000)
-
 def solution(n, lighthouse):
-    graph = defaultdict(list)
+    graph = [[] for _ in range(n+1)]
+    for s, e in lighthouse:
+        graph[s].append(e)
+        graph[e].append(s)
     
-    for a, b in lighthouse:
-        graph[a].append(b)
-        graph[b].append(a)
+    visited = [0] * (n+1)
+    dp = [[0,0] for _ in range(n+1)]
     
-    dp = [[0, 0] for _ in range(n + 1)] # 서브트리 루트 노드가 꺼지거나 켜진 경우 서브트리의 최소 등대 수
-    visited = [0] * (n + 1)
-
     def dfs(node):
         visited[node] = 1
-        dp[node][0] = 0  # node off
-        dp[node][1] = 1  # node on
+        dp[node][0] = 0 # 현재 노드가 꺼진 경우 최소 등대 수
+        dp[node][1] = 1 # 현재 노드가 켜진 경우 최소 등대 수
         for child in graph[node]:
             if not visited[child]:
                 dfs(child)
-                dp[node][0] += dp[child][1] # 현재 노드가 꺼지면 -> 자식 노드가 무조건 켜져야 함
-                dp[node][1] += min(dp[child][0], dp[child][1]) # 현재 노드 켜짐 -> 두 상태 중 최솟값 선택
-
+                dp[node][0] += dp[child][1]
+                dp[node][1] += min(dp[child][0], dp[child][1])
     dfs(1)
-    return min(dp[1][0], dp[1][1])
+    return min(dp[1][0], dp[1][1]) # 루트노드기준
