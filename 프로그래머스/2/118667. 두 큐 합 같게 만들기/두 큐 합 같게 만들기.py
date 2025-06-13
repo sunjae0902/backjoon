240d1d0b8b861a5bb5413391cc69f830a28301cc
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-06-13 23:48:56 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 56.11 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,31 +1,26 @@
 from collections import deque
 
 def solution(queue1, queue2):
-    q1 = deque(queue1)
-    q2 = deque(queue2) # 양방향 덱
-    sum1, sum2 = sum(q1), sum(q2)
-    total = sum1 + sum2
-
-    if total % 2 != 0:  # 합이 홀수면 x
+    answer = 0
+    d1, d2 = deque(queue1), deque(queue2)
+    sum1, sum2 = sum(d1), sum(d2)
+    
+    if (sum1 + sum2) % 2 != 0:
         return -1
-
-    target = total // 2
-    max_ops = len(queue1) * 3 # 모든 수를 교환해도 안 되는 경우가 상한선
-    ops = 0
-    p1, p2 = 0, 0 
-
-    while ops <= max_ops: # 균형이 맞을 때까지 연산 반복
+    
+    target = (sum1+sum2) //2 
+    max_ops = len(d1) * 3
+    
+    while answer <= max_ops:
         if sum1 == target:
-            return ops
-        elif sum1 > target: # 더 큰 쪽에서 제거 
-            val = q1.popleft()
-            sum1 -= val
-            q2.append(val)
+            return answer
+        if sum1 > target:
+            v = d1.popleft()
+            d2.append(v)
+            sum1 -= v
         else:
-            val = q2.popleft()
-            sum2 -= val
-            q1.append(val)
-            sum1 += val
-        ops += 1
-
+            v = d2.popleft()
+            d1.append(v)
+            sum1 += v
+        answer += 1 
     return -1
