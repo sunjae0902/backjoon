ff084371d95b70697aca64c55934f20153cd9d51
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-03-03 10:37:54 +0900

    [level 3] Title: 가장 먼 노드, Time: 7040.53 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,24 +1,21 @@
-import heapq as hq  
+from collections import deque
+
 def solution(n, edge):
-    answer = 0
-    arr = [[] for _ in range(n+1)]
+    answer = []
+    graph = [[] for _ in range(n+1)]
     visited = [0 for _ in range(n+1)]
     visited[1] = 1
+    q = deque([(0, 1)]) # 최단거리, N번노드까지
     for s,e in edge:
-        arr[s].append(e)
-        arr[e].append(s)
-    
-    q = [(0,1)]
-    dist = [0 for _ in range(n+1)]
+        graph[s].append(e)
+        graph[e].append(s)
     while q:
-        shortest, cur = hq.heappop(q)
-        dist[cur] = shortest
-        for dest in arr[cur]:
-            if not visited[dest]:
-                visited[dest] = 1
-                hq.heappush(q, (shortest + 1, dest))
-    max_dist = max(dist)
-    for d in dist:
-        if d == max_dist:
-            answer += 1
-    return answer
+        dist, node = q.popleft()
+        answer.append(dist)
+        for n in graph[node]:
+            if visited[n]:
+                continue
+            visited[n] = 1
+            q.append((dist+1, n))
+
+    return len([i for i in answer if i == max(answer)])
