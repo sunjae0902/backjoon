cedfc0167c698f644fc64bf83e07587249b806a2
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-03-09 16:04:25 +0900

    [level 3] Title: 기둥과 보 설치, Time: 88.93 ms, Memory: 9.74 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -1,59 +1,44 @@
-def check_0(x, y, s):   # 기둥
-    # 바닥 위
-    if y == 0:
-        return True
-    # 아래에 기둥
-    if (x, y-1, 0) in s:
-        return True
-    # 왼쪽에 보
-    if (x-1, y, 1) in s:
-        return True
-    # 오른쪽에 보
-    if (x, y, 1) in s:
-        return True
-    return False
+# 0 기둥
+# 1 보
 
-
-def check_1(x, y, s):   # 보
-    # 왼쪽 아래 기둥
-    if (x, y-1, 0) in s:
+def possible_beam(x,y,s):
+    if (x,y-1,0) in s or (x+1,y-1,0) in s:
         return True
-    # 오른쪽 아래 기둥
-    if (x+1, y-1, 0) in s:
-        return True
-    # 양쪽에 보
     if (x-1, y, 1) in s and (x+1, y, 1) in s:
         return True
     return False
 
+def possible_column(x,y,s):
+    if y == 0:
+        return True
+    if (x-1,y,1) in s or (x,y,1) in s:
+        return True
+    if (x, y-1, 0) in s:
+        return True
+    return False
 
-def possible(s): 
-    for x, y, a in s: # 주변만 체크하는게 아니라, 모든 구조물에 대해서 검사해야함
-        if a == 0: # 종류 검사
-            if not check_0(x, y, s):
+def possible(s):
+    for x,y,a in s:
+        if a:
+            if not possible_beam(x,y,s):
                 return False
         else:
-            if not check_1(x, y, s):
+            if not possible_column(x,y,s):
                 return False
     return True
 
-
 def solution(n, build_frame):
-    answer = set()   # (x, y, a)
-
-    for x, y, a, b in build_frame:
-        if b == 1:   # 설치
-            answer.add((x, y, a)) # 일단 추가하고, 조건 검사 후 맞지 않으면 제거
-
+    answer = set()
+    
+    for x,y,a,b in build_frame:
+        if b:
+            answer.add((x,y,a))
             if not possible(answer):
-                answer.remove((x, y, a))
-
-        else:        # 삭제
-            answer.remove((x, y, a)) # 일단 빼고, 조건 검사 후 맞지 않으면 다시 추가
-
+                answer.remove((x,y,a))
+        else:
+            answer.remove((x,y,a))
             if not possible(answer):
-                answer.add((x, y, a))
-
-    result = list(map(list, answer)) # list로 변환
-    result.sort(key=lambda x: (x[0], x[1], x[2]))
-    return result
+                answer.add((x,y,a))
+                
+    answer = sorted(list(answer), key=lambda x: (x[0], x[1], x[2]))
+    return answer
