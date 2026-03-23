89344da6a5095f4339072ade1684b70306b3791c
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-03-23 14:17:00 +0900

    [Gold IV] Title: 뱀, Time: 60 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -5,38 +5,33 @@ input = sys.stdin.readline
 
 n = int(input())
 k = int(input())
-move = [(0,1), (1,0), (0,-1), (-1,0)] # 오른쪽, 아래, 왼쪽, 위 (시계방향 90도 회전 순서)
-board = [[0] * n for _ in range(n)]
-
+board = [[0 for _ in range(n)] for _ in range(n)]
+snake = deque([(0, 0)])
+move = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+dir_idx = 0
 for _ in range(k):
-    i, j = map(int, input().split())
-    board[i-1][j-1] = 1
+    r, c = map(int, input().split())
+    board[r-1][c-1] = 1 # 사과
 
+rotate = {} # 회전 방향
 l = int(input())
-rotate = {}
-for _ in range(l): # s초에 d방향
-    s, d = input().split()
+for _ in range(l):
+    s, d = map(str, input().split())
     rotate[int(s)] = d
 
-sec = 0
-dir_idx = 0
-snake = deque([(0,0)]) # 머리, 몸통..,(꼬리)
-
+answer = 0
 while True:
-    sec += 1
-    head = snake[0] # 현재 머리 위치
-    r, c = head[0] + move[dir_idx][0], head[1] + move[dir_idx][1]
-    if sec in rotate:
-        if rotate[sec] == 'D':
-            # 순환하기 때문에 (% 4)로 나머지정리
-            dir_idx = (dir_idx+1) % 4 # 현재 방향에서 오른쪽으로 회전했을 때 방향
-        else:
-            dir_idx = (dir_idx-1) % 4
-    if not (0 <= r < n and 0 <= c < n) or (r, c) in snake:
-        print(sec)
+    answer += 1
+    head = snake[0]
+    nx = head[0] + move[dir_idx][0]
+    ny = head[1] + move[dir_idx][1]
+    if answer in rotate:
+        dir_idx = (dir_idx + 1) % 4 if rotate[answer] == "D" else (dir_idx - 1) % 4
+    if not (0 <= nx < n and 0 <= ny < n) or (nx, ny) in snake:
         break
-    snake.appendleft((r, c)) # 새 머리 
-    if board[r][c] == 1:
-        board[r][c] = 0
+    snake.appendleft((nx, ny))
+    if board[nx][ny] == 1:
+        board[nx][ny] = 0
     else:
         snake.pop()
+print(answer)
