69519645c688668f4331862e3b0ee5adf0339fab
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-03-23 15:01:50 +0900

    [Gold V] Title: 수 고르기, Time: 164 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,15 +1,21 @@
 import sys
 input = sys.stdin.readline
-n, m = map(int, input().split())
+n,m = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
+
 arr.sort()
-s,e = 0,0
+s,e = 0, 1
 answer = int(2e9)
-# 현재만 보고 판단해야 함
-while s < n and e < n: 
-    if arr[e] - arr[s] < m:
-        e += 1
-    else:
-        answer = min(answer, arr[e] - arr[s])
+
+while s < n and e < n:
+    diff = arr[e] - arr[s]
+    if diff == m:
+        answer = m
+        break
+    elif diff > m:
+        answer = min(answer, diff)
         s += 1
+    else:
+        e += 1
+
 print(answer)
