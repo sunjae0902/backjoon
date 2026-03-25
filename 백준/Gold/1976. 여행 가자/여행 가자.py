a2ebd51267b225f32ca420220a5f2b23889d64c0
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-03-25 15:43:46 +0900

    [Gold IV] Title: 여행 가자, Time: 64 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -5,21 +5,25 @@ from collections import deque
 n = int(input()) # 도시 수 
 m = int(input()) # 여행 계획 도시 수
 graph = [[] for _ in range(n+1)]
+parent = [i for i in range(n + 1)]
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    pa, pb = find(a), find(b)
+    if pa != pb:
+        parent[pa] = pb
+
 for i in range(1, n+1):
     tmp = list(map(int, input().split()))
     for j in range(1, n+1):
-        if tmp[j-1] == 1:
-            graph[i].append(j)
-travel = deque(list(map(int, input().split())))
-visited = [0 for _ in range(n+1)]
-q = deque([travel[0]])
-visited[travel[0]] = 1
-flag = 0
-while q:
-    cur = q.popleft()
-    for nxt in graph[cur]:
-        if not visited[nxt]:
-            visited[nxt] = 1
-            q.append(nxt)
-print("YES" if all(visited[n] for n in travel) else "NO")
+        if tmp[j-1] == 1: # 연결
+            union(i, j)
             
+travel = deque(list(map(int, input().split())))
+root = find(travel[0])
+
+print("YES" if all(find(parent[n]) == root for n in travel) else "NO") 
