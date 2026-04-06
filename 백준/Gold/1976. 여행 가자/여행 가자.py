4b26226435a80ed0d883655902a60114da5aa9c2
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-04-06 21:55:31 +0900

    [Gold IV] Title: 여행 가자, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,14 +1,12 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
+n = int(input())
+m = int(input())
 
-n = int(input()) # 도시 수 
-m = int(input()) # 여행 계획 도시 수
-graph = [[] for _ in range(n+1)]
-parent = [i for i in range(n + 1)]
+parent = [i for i in range(n+1)] 
 
 def find(x):
-    if parent[x] != x:
+    if x != parent[x]:
         parent[x] = find(parent[x])
     return parent[x]
 
@@ -17,13 +15,11 @@ def union(a, b):
     if pa != pb:
         parent[pa] = pb
 
-for i in range(1, n+1):
+for i in range(n):
     tmp = list(map(int, input().split()))
-    for j in range(1, n+1):
-        if tmp[j-1] == 1: # 연결
-            union(i, j)
-            
-travel = deque(list(map(int, input().split())))
-root = find(travel[0])
-
-print("YES" if all(find(parent[n]) == root for n in travel) else "NO") 
+    for j in range(n):
+        if tmp[j]:
+            union(i+1, j+1)
+path = list(map(int, input().split()))
+root = find(path[0])
+print("YES" if all(find(p) == root for p in path) else "NO")
