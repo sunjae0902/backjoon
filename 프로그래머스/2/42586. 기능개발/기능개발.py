2cc8ee4e35954f53ea862228515613d244a4c5c7
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-05-06 13:26:11 +0900

    [level 2] Title: 기능개발, Time: 1.20 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,16 +1,21 @@
 def solution(progresses, speeds):
-    answer = []
-    n = len(progresses)
     days = []
+    n = len(progresses)
+
     for i in range(n):
-        days.append((100-progresses[i]-1) // speeds[i] + 1)
-    s = [days[0]]
-    for i in range(1, n):
-        if days[i] <= max(s):
-            s.append(days[i])
-        else:
-            answer.append(len(s))
-            s = [days[i]]
-    if s:
-        answer.append(len(s))
-    return answer
+        progresses[i] = [progresses[i], speeds[i]]
+    
+    st = progresses[::-1]
+
+    while True:
+        cnt = 0
+        if sum(days) == n:
+            break
+        while st and st[-1][0] >= 100:
+            st.pop()
+            cnt += 1
+        if cnt:
+            days.append(cnt)
+        for j in range(len(st)):
+            st[j][0] += st[j][1]
+    return days
