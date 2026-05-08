90d878580a68571ef5a05d1973393827eef236b7
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-05-08 16:40:46 +0900

    [level 2] Title: 전화번호 목록, Time: 69.51 ms, Memory: 29.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 30.7 MB, 시간: 108.89 ms
+메모리: 29.6 MB, 시간: 69.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 10일 21:52:28
+2026년 05월 08일 16:40:42
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,11 @@
+from collections import defaultdict
+
 def solution(phone_book):
-    answer = True
     phone_book.sort()
-   
-    for i in range(1, len(phone_book)):
-        if phone_book[i][:len(phone_book[i-1])] == phone_book[i-1]:
-            answer = False
-            break
-    return answer
+    d = defaultdict(str)
+    
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    
+    return True
