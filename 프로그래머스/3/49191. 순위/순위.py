6e1b2d6170cf57c5c3758ecdc54c88b87f771102
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-05-29 19:19:28 +0900

    [level 3] Title: 순위, Time: 39.39 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,18 +1,22 @@
+# 06:58 시작
+
 def solution(n, results):
     answer = 0
-    arr = [[0 for _ in range(n+1)] for _ in range(n+1)]
-    for a, b in results:
-        arr[a][b] = 1
-        arr[b][a] = -1 
-        
+    # A < B, B < C이면 A<B<C
+    info = [[0 for _ in range(n+1)] for _ in range(n+1)] # 1: 이김, -1: 짐, 0: 모름
+    for w, l in results:
+        info[w][l] = 1
+        info[l][w] = -1
+    
+    for i in range(n+1):
+        for j in range(n+1):
+            for k in range(n+1):
+                if info[j][i] == 1 and info[i][k] == 1:
+                    info[j][k] = 1
+                    info[k][j] = -1
     for i in range(1, n+1):
-        for j in range(1, n+1):
-            for k in range(1, n+1):
-                if arr[j][i] == 1 and arr[i][k] == 1:
-                    arr[j][k] = 1
-                    arr[k][j] = -1
-    for i in range(1, n+1):
-        w, l = arr[i].count(1), arr[i].count(-1)
-        if w + l == n-1:
+        w, l = info[i].count(1), info[i].count(-1)
+        if w+l == n-1:
             answer += 1
+    
     return answer
