dd9a3410817424bc5ab4d1fbaeb8549f74bddc03
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-05-29 19:52:15 +0900

    [level 3] Title: 여행경로, Time: 309.08 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,21 +1,23 @@
-from collections import defaultdict, deque
+from collections import deque, defaultdict
 
 def solution(tickets):
-    dic = defaultdict(list)
-    
+    info = defaultdict(list)
+
     for s, e in sorted(tickets):
-        dic[s].append(e)
-    
-    q = deque([("ICN", ["ICN"], set())])
-    
+        info[s].append(e)
+
+    q = deque([(["ICN"], set())]) # 출발지, 도착지 경로 저장
+
     while q:
-        now, path, used = q.popleft()
-        
+        path, used = q.popleft()
+
         if len(used) == len(tickets):
             return path
-        
-        for i, next_dest in enumerate(dic[now]):
-            if (now, i) not in used: 
-                q.append((next_dest, path + [next_dest], used | {(now, i)}))
-    
+
+        cur = path[-1]
+
+        for i, nxt in enumerate(info[cur]):
+            if (cur, nxt, i) in used:
+                continue
+            q.append((path + [nxt], used | {(cur, nxt, i)}))
     return []
