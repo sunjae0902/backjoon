4a2c946fc65683b2489168fe80d91efca2138a4e
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-05-30 20:03:13 +0900

    [level 3] Title: 정수 삼각형, Time: 24.49 ms, Memory: 20.1 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,17 +1,14 @@
 def solution(triangle):
-    answer = 0
     n = len(triangle)
-    dp = [[0 for _ in range(i+1)] for i in range(n)] # 마지막 기준 최댓값 저장
-    dp[0][0] = triangle[0][0] 
+    dp = [[0 for _ in range(len(triangle[i]))] for i in range(n)] 
+    dp[0][0] = triangle[0][0]
+        
     for i in range(1, n):
-        for j in range(i+1):
+        for j in range(len(triangle[i])):
             if j == 0:
-                dp[i][j] = triangle[i][j] + dp[i-1][0]
-                continue
-            elif j == i:
-                dp[i][j] = triangle[i][j] + dp[i-1][j-1]
-                continue
-            dp[i][j] = triangle[i][j] + max(dp[i-1][j], dp[i-1][j-1])
-    answer = max(dp[n-1])
-
-    return answer
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j == len(triangle[i])-1:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+    return max(dp[-1])
