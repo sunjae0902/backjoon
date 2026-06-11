408ad27958cacfe62ecf6ff780f337ad6eaef159
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-06-11 14:40:05 +0900

    [level 3] Title: 섬 연결하기, Time: 0.05 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,26 +1,26 @@
-import heapq
-
 def solution(n, costs):
+    parent = list(range(n))
+
+    def find(x):
+        if parent[x] != x:
+            parent[x] = find(parent[x])
+        return parent[x]
+
+    def union(a, b):
+        a = find(a)
+        b = find(b)
+
+        if a != b:
+            parent[a] = b
+            return True # 합 집합인거고
+        return False # 못합한거
+
+    costs.sort(key=lambda x: x[2]) # 비용 적은 순 정렬
+
     answer = 0
-    arr = [[] for _ in range(n)]
-    for s, e, w in costs:
-        arr[s].append([e,w])
-        arr[e].append([s,w])
-    
-    visited = set()
-    q = [[0, 0]] # 가중치, 시작 노드
-    count = 0
-    while q:
-        weight, now = heapq.heappop(q)
-        if now in visited:
-            continue
-        visited.add(now) # 가장 최소인 노드 방문 표시.
-        answer += weight
-        count += 1
-        if count == n:
-            break
-        for v, w in arr[now]:
-            if v not in visited:
-                heapq.heappush(q, [w, v])
-    
+
+    for a, b, cost in costs:
+        if union(a, b): # 합해지면
+            answer += cost
+
     return answer
