9baae022f7783bdf21673c16134d4c7362b2a414
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-07-14 21:05:45 +0900

    [level 3] Title: 입국심사, Time: 297.04 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,17 +1,15 @@
 def solution(n, times):
     answer = 0
-    s, e = 1, max(times) * n
-
+    s, e = 0, max(times) * n
+    
     while s <= e:
-        people = 0
+        finish = 0
         m = (s+e) // 2
         for time in times:
-            people += m // time
-            if people >= n:
-                break
-        if people >= n:
+            finish += m // time
+        if finish >= n:
             answer = m
-            e = m - 1
+            e = m -1
         else:
             s = m + 1
     return answer
